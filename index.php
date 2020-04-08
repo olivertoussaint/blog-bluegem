d7cc94c0eb2f44bbbb9cef15c9c8363f7b4d8d19
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-04-08 21:55:26 +0200

    update index

--- index.php
+++ index.php
@@ -1,28 +1,132 @@
 <?php
 session_start();
 
-require_once __DIR__.'/vendor/autoload.php';
+require 'vendor/autoload.php';
 
-use Projet\controller\UserController;
+use Projet\controller\backend\AdminController;
+use Projet\controller\frontend\UserController;
 
 
 try {
-	if (isset($_GET['action']))	{	
-		if ($_GET['action'] == 'signIn') { 
+	if (isset($_GET['action']))	{
+		if ($_GET['action'] == 'listNews') {			
+			$controller = new UserController();
+		    $controller -> listNews();
+
+		} elseif ($_GET['action'] == 'latestNews') {
+			if (isset($_GET['id']) && $_GET['id'] > 0 && is_numeric($_GET['id'])) {
+				$controller = new UserController();
+				$controller -> latestNews();
+			}
+			else {
+				throw new Exception('Aucun identifiant de news envoyé');
+			}
+		
+		} elseif ($_GET['action'] == 'addMember') {
+			if (!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['passwordConfirm'])) {
+
+				if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+						if ($_POST['password'] == $_POST['passwordConfirm']) {
+					
+					$controller = new UserController();
+					$controller -> addMember(strip_tags($_POST['pseudo']), strip_tags($_POST['password']), strip_tags($_POST['email']));
+				}
+				else {
+						throw new Exception('Les deux mots de passe ne correspondent pas.');
+				}
+		} else {
+				  throw new Exception('Pas d\'adresse email valide.');
+		}
+	} else {
+			  throw new Exception('Tous les champs ne sont pas remplis !');
+	}
+
+		} elseif ($_GET['action'] == 'profile') {
+			// $msg = "";
+            // $css_class = "";
+			$controller = new UserController();
+			$controller -> profile();
+		
+		} elseif ($_GET['action'] == 'updateProfile') {
+			//TODO
+			
+		} 
+		
+		elseif ($_GET['action'] == 'addComment') {
+			if (isset($_GET['id']) && $_GET['id'] > 0) {
+				if (isset($_SESSION['id']) && !empty($_POST['comment'])) {
+					$controller = new UserController();
+					$controller ->addComment($_GET['id'], $_SESSION['id'], $_POST['comment']);
+
+				} else {
+					throw new Exception('Tous les champs doivent être remplis !');
+					}
+				} else {
+				  throw new Exception('Aucun identifiant de billet envoyé');
+		}  
+
+		} elseif ($_GET['action'] == 'loginSubmit') {
+			$controller = new UserController();
+			$controller -> loginSubmit(strip_tags($_POST['pseudo']), strip_tags($_POST['password']));
+
+		} elseif ($_GET['action'] == 'signIn') {
 			$controller = new UserController();
 			$controller -> signIn();
+
 		} elseif ($_GET['action'] == 'signUp') {
 			$controller = new UserController();
 			$controller -> signUp();
-		}
-	} else // si je n'ai pas de $_GET ['action']
-	{
-		require ('src/view/frontend/homeView.php');
+		
+		} elseif ($_GET['action'] == 'reporting') {
+			$controller = new UserController();
+			$controller->newsReport($_GET['id'], $_SESSION['pseudo']);
 
+		} elseif ($_GET['action'] == 'admin') {
+			if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
+				$adminController = new AdminController();
+				$adminController -> admin();
+			} else {
+				throw new Exception('Vous n\'êtes pas autorisé à accéder à cette partie du site');
+                header("Location: index.php");
+                exit;            
+			}
+
+		} elseif ($_GET['action'] == 'validateComment') {
+			$controller = new UserController();
+			// $controller -> newsReport();
+			
+		}  elseif ($_GET['action'] == 'deleteNews') {
+			// removeNews($_GET['id']);// ADMIN
+
+
+		} elseif ($_GET['action'] == 'termsOfUse') {
+			$controller = new UserController();
+			$controller -> termsOfUse();
+
+		} elseif ($_GET['action'] == 'logout') {
+			$controller = new UserController();
+			$controller -> logout();
+		
+		} elseif ($_GET['action'] == 'legalNotices') {
+			$controller = new UserController();
+			$controller -> legalNotices();
+
+		} elseif ($_GET['action'] == 'contactMe') {
+			$controller = new UserController();
+			$controller -> contactMe();
+		}
+	} else 
+	{
+
+		$controller = new Usercontroller;
+		$controller -> index();
+		// $controller -> listNews();
 	}
 }
 
-catch (Exception $e) {
-    $errorMessage = $e->getMessage();
-    require('src/view/frontend/errorView.php');
+
+catch (Exception $e) 
+{
+    die('Erreur : '.$e->getMessage());
+    
 }
