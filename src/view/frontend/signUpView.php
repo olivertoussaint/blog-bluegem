30166a40700227b774790cbfb8d9e3716d4f56e3
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-04-08 22:02:38 +0200

    update view

--- src/view/frontend/signUpView.php
+++ src/view/frontend/signUpView.php
@@ -6,42 +6,49 @@
         <div class="col-lg-6 col-md-8">
             <div class="alert blue-grey lighten-5">
                 <h2 class="text-center">Inscrivez-vous</h2>
-                <form action="index.php?=register" method="POST" id="form-registration">
-                <div id="registration">
+                <form action="index.php?action=addMember" method="POST">
                     <div class="form-group">
-                        <label for="registration_username" class="required">Pseudo</label>
-                        <input type="text" id="registration_username" name="registration[username]" required="required" placeholder="Votre pseudo ..." class="form-control" />
+                        <label for="pseudo" class="required">Pseudo</label>
+                        <input type="text" id="pseudo" name="pseudo" required="required" placeholder="Votre pseudo ..." class="form-control" />
                     </div>
                     <div class="form-group">
-                        <label for="registration_email" class="required">Email</label>
-                        <input type="email" id="registration_email" name="registration[email]" required="required" placeholder="Votre email ..." class="form-control" />
+                        <label for="email" class="required">Email</label>
+                        <input type="email" id="email" name="email" required="required" placeholder="Votre email ..." class="form-control" />
                     </div>
-                    <div class="form-group">
-                        <label for="registration_picture">Photo de profil</label>
-                        <input type="text" id="registration_picture" name="registration[picture]" placeholder="URL de votre avatar (optionnel) ..." inputmode="url" class="form-control" /></div>
+
                         <div class="form-group">
-                            <label for="registration_hash" class="required">Mot de passe</label>
-                            <input type="password" id="registration_hash" name="registration[hash]" required="required" placeholder="Choisissez un mot de passe !" class="form-control" /></div>
-                            <div class="form-group">
-                                <label for="registration_passwordConfirm" class="required">Confirmation de mot de passe</label>
-                                <input type="password" id="registration_passwordConfirm" name="registration[passwordConfirm]" required="required" placeholder="Veuillez confirmer votre mot de passe !" class="form-control" />
-                            </div>
-                           
-                </div>
-                <button class="btn btn-info my-4 btn-block" type="submit">S'inscrire</button>
+                            <label for="password" class="required">Mot de passe</label>
+                            <input type="password" id="password" name="password" required="required" placeholder="Choisissez un mot de passe" class="form-control" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="passwordConfirm" class="required">Confirmation de mot de passe</label>
+                            <input type="password" id="passwordConfirm" name="passwordConfirm" required="required" placeholder="Veuillez confirmer votre mot de passe" class="form-control" />
+                        </div>
+
+
+                    <div class="form-group">
+                        <button type="submit" name="submit" class="btn btn-info my-4 btn-block" >S'inscrire</button>
+                    </div>
                 <hr>
+                <div class="g-recaptcha" 
+                    data-sitekey="6Le6WOIUAAAAAMDCirIT8f92_JR4SYF_9LKZvheV">
+                </div>
+                
                 <p>En cliquant sur
                         <em>S'inscrire</em> vous acceptez nos
-                        <a href="" target="_blank">conditions d'utilisation</a>
+                        <a href="index?action=termsOfUse" target="_blank">conditions d'utilisation</a>
                 </p>
                 </form>
+           
+
             </div>
             <div class="row">
                 <div class="col-7">
             <p>Déjà membre&nbsp;?&nbsp;cliquez<a href="index.php?action=signIn" title="Cliquez ici pour vous logger">  ici</a></p>
             </div>
             <div class="col-5">
-                <p><a href="index.php" title="retour à la page d'accueil">Retour à la page d'accueil</a></p>
+                <p><a href="index" title="retour à la page d'accueil">Retour à la page d'accueil</a></p>
             </div>    
         </div>
     </div>
