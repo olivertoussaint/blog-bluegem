51082b3e532085b98d561b00cff1515519b545a4
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-05-17 01:16:23 +0200

    small modification

--- src/public/template/template_dashboard.php
+++ src/public/template/template_dashboard.php
@@ -38,6 +38,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/js/mdb.min.js"></script>
+    <!-- JS -->
     <script type="text/javascript" src="./src/public/assets/js/admin.js"></script> 
       
 </body>
