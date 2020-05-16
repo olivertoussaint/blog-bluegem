4d695a6f755ecb5de62a0090b1e694da627faa99
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-05-17 01:14:52 +0200

    small modification

--- src/public/layout/navHeader.php
+++ src/public/layout/navHeader.php
@@ -9,7 +9,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=/index">Accueil <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="/index">Accueil <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                            <a class="nav-link" href="index.php?action=listNews">Le blog</a>
