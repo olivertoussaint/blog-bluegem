157b20a6681d1ca1b5ed63b65b267313de20957a
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-07-27 00:26:55 +0200

    add select2.js

---
+++ src/public/assets/js/select2.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#categories").select2({
+        theme: "classic"
+    });
+    
+    $("#keywords").select2({
+        placeholder: 'Selectionnez une sous-catégorie',
+        theme: "classic"
+    });
+})
