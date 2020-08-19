fbb5d8a39fa9ae11d4f873fd4c2a8db402d19c4a
Olivier Toussaint <toussaint.olivier66@gmail.com>
2020-08-20 00:43:55 +0200

    little update of frontend view

--- src/view/frontend/template.php
+++ src/view/frontend/template.php
@@ -2,10 +2,17 @@
 <html lang="fr">
 
 <head>
-    <meta charset="utf-8" />
+<meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content=" BlueGem est un forum pour toutes celles et ceux qui s'interrogent sur les bouleversements que subit notre planète." />
+    <link rel="canonical" href="http://projet5.oliviertoussaint.fr">
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://projet5.oliviertoussaint.fr/" />
+    <meta property="og:site_name" content="BlueGem" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <link rel="shortcut icon" href="./src/public/assets/ico/favicon.ico" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!-- Bootstrap core CSS -->
