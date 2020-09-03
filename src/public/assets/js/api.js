f2ba4121d5613884b8c6619b5e5f621ecd4bbf2c
olivertoussaint <toussaint.olivier66@gmail.com>
2020-09-03 17:35:46 +0200

    little update

--- src/public/assets/js/api.js
+++ src/public/assets/js/api.js
@@ -5,12 +5,6 @@ let apiCall = function(city){
 
 	function capitalize(str){
 		return str[0].toUpperCase() + str.slice(1);
-	}
-	function addZero(i) {
-		if (i < 10) {
-			i = "0" + i;
-		}
-		return i;
 	}	
 	
 	function refreshTime() {
@@ -38,6 +32,7 @@ let apiCall = function(city){
 		let feels_like = Math.round(data.main.feels_like);
 		let visibility = (data.visibility/1000);
 		const weatherIconId = data.weather[0].icon;
+		// const loadingContainer = document.querySelector('overlay-icon-loading');
 
 		console.log(data);
 
@@ -51,18 +46,21 @@ let apiCall = function(city){
 		document.querySelector('#wind').innerHTML = "<img src=\"src/public/icons/svg/blowing" +".svg\">" + `${wind}`+'&nbsp;' + 'km/h';
 		document.querySelector('#pressure').innerHTML = "<img src=\"src/public/icons/svg/barograph" +".svg\">" + `${pressure}`+','+0+0+'&nbsp;'+'hPa';
 		document.body.className = data.weather[0].main.toLowerCase();
+		// loadingContainer.classList.add('disappear');
 	})
+	
 	)
 
 	.catch(function() {
 		// catch any errors
-	});
+	});	
 }
 
 	document.querySelector('form').addEventListener('submit', function(e) {
 	e.preventDefault();
 	let city = document.querySelector('#inputCity').value;
 	apiCall(city);
+
 });
 
 apiCall('goussainville');
